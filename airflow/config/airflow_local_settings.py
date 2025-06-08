1435b7095b4628e8f7ad99050071bd6e06b7352e
Christopher <cromanillos99@cromDebDesktop.myfiosgateway.com>
2025-06-07 23:26:55 -0400

    airflow and docker settings updated

--- airflow/config/airflow_local_settings.py
+++ airflow/config/airflow_local_settings.py
@@ -1,11 +1,29 @@
-# airflow_local_settings.py
+# config/airflow_local_settings.py
 
 from pythonjsonlogger import jsonlogger
+import logging
+import sys
+
+formatter = jsonlogger.JsonFormatter(
+    '%(asctime)s %(levelname)s %(name)s %(message)s',
+    json_ensure_ascii=False
+)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+
+for logger_name in (
+    'airflow', 'airflow.task', 'airflow.task_runner', 'airflow.jobs.scheduler_job'
+):
+    airflow_logger = logging.getLogger(logger_name)
+    airflow_logger.handlers.clear()
+    airflow_logger.setLevel(logging.INFO)
+    airflow_logger.addHandler(handler)
 
 LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
-
     'formatters': {
         'json': {
             '()': jsonlogger.JsonFormatter,
@@ -13,7 +31,6 @@ LOGGING_CONFIG = {
             'json_ensure_ascii': False
         }
     },
-
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
@@ -21,12 +38,10 @@ LOGGING_CONFIG = {
             'stream': 'ext://sys.stdout',
         }
     },
-
     'root': {
         'handlers': ['console'],
         'level': 'INFO',
     },
-
     'loggers': {
         'airflow': {
             'handlers': ['console'],
