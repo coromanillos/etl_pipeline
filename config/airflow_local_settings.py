74cc4ed97341fa8f2fcebd62a6bbfec2274813da
Christopher <cromanillos99@cromDebDesktop.myfiosgateway.com>
2025-06-09 23:53:12 -0400

    local settings changse

--- config/airflow_local_settings.py
+++ config/airflow_local_settings.py
@@ -1,48 +1,60 @@
 # config/airflow_local_settings.py
 
 from pythonjsonlogger import jsonlogger
+import logging
 
+# Define a reusable JSON formatter class
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def __init__(self):
+        super().__init__(
+            fmt="%(asctime)s %(levelname)s %(name)s %(filename)s %(funcName)s %(lineno)d %(message)s",
+            json_ensure_ascii=False
+        )
+
+# Logging configuration dictionary
 LOGGING_CONFIG = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'json': {
-            '()': jsonlogger.JsonFormatter,
-            'format': '%(asctime)s %(levelname)s %(name)s %(message)s',
-            'json_ensure_ascii': False,
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    "formatters": {
+        "json": {
+            "()": CustomJsonFormatter,
         }
     },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'json',
-            'stream': 'ext://sys.stdout',
+
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "json",
+            "stream": "ext://sys.stdout",
         }
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
+
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
     },
-    'loggers': {
-        'airflow': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
+
+    "loggers": {
+        "airflow": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
         },
-        'airflow.task': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
+        "airflow.task": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
         },
-        'airflow.task_runner': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
+        "airflow.task_runner": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
         },
-        'airflow.jobs.scheduler_job': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
+        "airflow.jobs.scheduler_job": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
         },
     }
 }
